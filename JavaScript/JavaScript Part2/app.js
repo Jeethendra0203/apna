1637de539e16d48a86d5f709b36b51a991b9be69
Jeethu <jeethendra0203@gmail.com>
2025-04-17 23:40:54 +0530

    JavaScript

---
+++ JavaScript/JavaScript Part2/app.js
@@ -0,0 +1,28 @@
+//Template Literals
+
+let pencilPrice = 10;
+let scalePrice = 15;
+// let output = "The total price is : " +(pencilPrice + scalePrice) + " Rupees.";
+// let output = `The total price is : ${pencilPrice + scalePrice} Rupees.`;
+// console.log(output);
+console.log(`The total price is : ${pencilPrice + scalePrice} Rupees.`);
+
+//Arithmetic Operators
+let a = 10;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+//Assignment Op
+b = a;
+console.log(b);
+
+//Unary Op
+console.log(a++);
+console.log(++a);
+
+//Comparison Op
+let age = 18;
+console.log(age >= 18);

4349d7e5722dcf3345a77b55cc71ae03fbf11aac
Jeethu <jeethendra0203@gmail.com>
2025-07-02 19:15:30 +0530

    react

--- React/React Part1/basic-react-app/src/product.jsx
+++ React/React Part1/basic-react-app/src/product.jsx
@@ -1,10 +1,17 @@
 import "./Product.css";
+import Price from "./Price";
 
-function Product() {
+
+function Product({ title, idx }) {
+    let oldPrices = ["12,000","10,999","1,999","660"]
+    let newPrices = ["9,999","8,880","999","350"]
+    let Description = [["8,000 DPI", "5 Programmable Buttons"], ["Intuitive surface", "Designed for ipad"], ["Intuitive surface","Designed for ipad"], ["Wireless", "Optical orientation"]]
     return (
     <div className="Product">
-        <h3>Product Title</h3>
-        <h5>Product Description</h5>
+        <h4>{title}</h4>
+        <p>{Description[idx][0]}</p>
+        <p>{Description[idx][1]}</p>
+        <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
     </div>
     );
 }

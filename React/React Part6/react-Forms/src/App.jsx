c0cfa1408974530e6b7659c4765b9a4b9920ec62
Jeethu <jeethendra0203@gmail.com>
2025-07-08 23:11:11 +0530

    React

--- React/React Part6/react-Forms/src/App.jsx
+++ React/React Part6/react-Forms/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 import Form from './Form'
+import CommentsForm from './CommentsForm'
+import Comment from './Comment'
+import Counter from './Counter';
+import Joker from './Joker';
 
 function App() {
   return (
     <>
-      <Form/>
+      {/* <Form/> */}
+      {/* <Comment/> */}
+      {/* <CommentsForm/> */}
+      {/* <Counter/> */}
+      <Joker/>
     </>
   )
 }
